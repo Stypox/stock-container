16e3f17190d85f382a4ab8442d03b4f8f2b7b3f4
Stypox <stypox@pm.me>
2018-12-23 14:03:10 +0100

    Addressed #1
    The private Container::remove() function is in charge of destroying an element and replacing it with the last one in the container, in order not to leave holes behind.
    Removed Data struct: now values and pointers to iterators are stored in two different places.
    Removed Iterator since it would be the exact same thing as a pointer.
    Now Handler holds a pointer to the Container to which its value belongs: Container::remove() has to be called when removing/destructing Iterators

--- include/stypox/stock_container.h
+++ include/stypox/stock_container.h
@@ -13,74 +13,21 @@ namespace stypox {
 	class M_StockContainerHandler;
 
 	template<class T>
-	class M_StockContainerData {
-		friend class StockContainer<T>;
-		friend class M_StockContainerHandler<T>;
-
-		M_StockContainerHandler<T>* iter;
-	public:
-		T value;
-	};
-
-	template<class T>
-	class M_StockContainerIterator {
-		friend class StockContainer<T>;
-	public:
-		using value_type = T;
-		using difference_type = ptrdiff_t;
-		using pointer = value_type*;
-		using reference = value_type&;
-		using iterator_category = std::random_access_iterator_tag;
-	private:
-		using data_type = M_StockContainerData<value_type>;
-
-		data_type* m_pointer;
-
-		M_StockContainerIterator(data_type* p) : m_pointer{p} {};
-	public:
-		M_StockContainerIterator(const M_StockContainerIterator&) = default;
-		M_StockContainerIterator(M_StockContainerIterator&&) = default;
-		M_StockContainerIterator& operator=(const M_StockContainerIterator&) = default;
-		M_StockContainerIterator& operator=(M_StockContainerIterator&&) = default;
-
-		reference operator*() const { return m_pointer->value; }
-		pointer operator->() const { return &m_pointer->value; }
-		reference operator[](difference_type n) const { return m_pointer[n].value; }
-
-		M_StockContainerIterator& operator++() { ++m_pointer; return *this; }
-		M_StockContainerIterator operator++(int) { return m_pointer++; }
-		M_StockContainerIterator& operator--() { --m_pointer; return *this; }
-		M_StockContainerIterator operator--(int) { return m_pointer--; }
-
-		M_StockContainerIterator& operator+=(difference_type n) { m_pointer += n; return *this; }
-		M_StockContainerIterator& operator-=(difference_type n) { m_pointer -= n; return *this; }
-
-		M_StockContainerIterator operator+(difference_type n) const { return m_pointer + n; }
-		friend M_StockContainerIterator operator+(difference_type n, const M_StockContainerIterator& iter) { return iter.m_pointer + n; }
-		M_StockContainerIterator operator-(difference_type n) const { return m_pointer - n; }
-		difference_type operator-(const M_StockContainerIterator& other) const { return m_pointer - other.m_pointer; }
-
-		bool operator==(const M_StockContainerIterator& other) const { return m_pointer == other.m_pointer; }
-		bool operator!=(const M_StockContainerIterator& other) const { return m_pointer != other.m_pointer; }
-		bool operator<(const M_StockContainerIterator& other) const { return m_pointer < other.m_pointer; }
-		bool operator>(const M_StockContainerIterator& other) const { return m_pointer > other.m_pointer; }
-		bool operator<=(const M_StockContainerIterator& other) const { return m_pointer <= other.m_pointer; }
-		bool operator>=(const M_StockContainerIterator& other) const { return m_pointer >= other.m_pointer; }
-	};
-
-	template<class T>
 	class M_StockContainerHandler {
 		friend class StockContainer<T>;
 	public:
 		using value_type = T;
+		using container_type = StockContainer<value_type>;
 	private:
-		using data_type = M_StockContainerData<value_type>;
+		value_type* m_value;
+		container_type* m_container;
 
-		data_type* m_data;
+		M_StockContainerHandler(value_type* value, container_type* container) :
+			m_value{value}, m_container{container} { m_container->updateCorrespondingHandler(m_value, this); }
 
-		M_StockContainerHandler(data_type* data) : m_data{data} { m_data->iter = this; }
-
-		void update(data_type* data) { m_data = data; }
+		void updateValue(value_type* value) { m_value = value; }
+		void updateContainer(container_type* container) { m_container = container; }
+		void setInvalid() { m_value = nullptr; }
 
 	public:
 		M_StockContainerHandler(const M_StockContainerHandler<value_type>& other) = delete;
@@ -92,16 +39,17 @@ namespace stypox {
 		~M_StockContainerHandler();
 		void remove();
 
-		value_type& operator*() const { return m_data->value; }
-		value_type* operator->() const { return &m_data->value; }
+		value_type& operator*() const { return *m_value; }
+		value_type* operator->() const { return m_value; }
 
-		bool operator==(nullptr_t) const { return m_data == nullptr; }
-		bool operator!=(nullptr_t) const { return m_data != nullptr; }
-		operator bool() const { return m_data != nullptr; }
+		bool operator==(nullptr_t) const { return m_value == nullptr; }
+		bool operator!=(nullptr_t) const { return m_value != nullptr; }
+		operator bool() const { return m_value != nullptr; }
 	};
 
 	template<class T>
 	class StockContainer {
+		friend class M_StockContainerHandler<T>;
 	public:
 		using value_type = T;
 		using size_type = size_t;
@@ -110,24 +58,27 @@ namespace stypox {
 		using const_reference = const value_type&;
 		using pointer = value_type*;
 		using const_pointer = typename std::pointer_traits<pointer>::template rebind<const value_type>;
-		using iterator = M_StockContainerIterator<value_type>;
-		using const_iterator = const M_StockContainerIterator<value_type>;
+		using iterator = pointer;
+		using const_iterator = const_pointer;
 		using reverse_iterator = std::reverse_iterator<iterator>;
 		using reverse_const_iterator = std::reverse_iterator<const_iterator>;
 		using handler = M_StockContainerHandler<value_type>;
 	private:
-		using data_type = M_StockContainerData<value_type>;
-
-		data_type* m_first,
-				 * m_space,
-				 * m_onePastLast;
+		value_type* m_first,
+				  * m_space,
+				  * m_onePastLast;
+		handler** m_firstHandler;
 
 		void reserveWithoutChecking(size_type newSize);
 		void growIfNeeded();
 
-		void updateAllIterators();
+		void updateIteratorValues();
+		void updateIteratorContainer();
+
+		void updateCorrespondingHandler(value_type* value, handler* newHandler) { m_firstHandler[value - m_first] = newHandler; }
+		void remove(value_type* value);
 	public:
-		StockContainer() : m_first{nullptr}, m_space{nullptr}, m_onePastLast{nullptr} {}
+		StockContainer() : m_first{nullptr}, m_space{nullptr}, m_onePastLast{nullptr}, m_firstHandler{nullptr} {}
 		StockContainer(const StockContainer& other) = delete;
 		StockContainer(StockContainer&& other);
 
@@ -146,21 +97,20 @@ namespace stypox {
 		std::vector<handler> append(InputIt first, InputIt last);
 		std::vector<handler> append(std::initializer_list<value_type> ilist) { return append(ilist.begin(), ilist.end()); }
 
-		data_type* data() const { return m_first; }
+		value_type* data() const { return m_first; }
 
 		bool empty() const { return m_space == m_first; }
 		size_type size() const { return m_space - m_first; }
 		size_type capacity() const { return m_onePastLast - m_first; }
-		size_type used() const;
 
 		void reserve(size_type newSize);
 		void shrink_to_fit();
 		void squash();
 
-		value_type& operator[](size_type n) { return m_first[n].value; }
-		value_type& at(size_type n) { if (n >= size()) throw std::out_of_range{"stypox::StockContainer::at()"}; return m_first[n].value; }
-		value_type& back() { return m_space[-1].value; }
-		value_type& front() { return m_first->value; }
+		value_type& operator[](size_type n) { return m_first[n]; }
+		value_type& at(size_type n) { if (n >= size()) throw std::out_of_range{"stypox::StockContainer::at()"}; return m_first[n]; }
+		value_type& back() { return m_space[-1]; }
+		value_type& front() { return *m_first; }
 
 		void swap(StockContainer<value_type>& other);
 
@@ -188,36 +138,33 @@ namespace stypox {
 
 	template<class T>
 	M_StockContainerHandler<T>::M_StockContainerHandler(M_StockContainerHandler<value_type>&& other) :
-		m_data{other.m_data} {
-		other.m_data = nullptr;
-		if (m_data)
-			m_data->iter = this;
+		m_value{other.m_value}, m_container{other.m_container} {
+		other.m_value = nullptr;
+		if (m_value)
+			m_container->updateCorrespondingHandler(m_value, this);
 	}
 	
 	template<class T>
 	auto M_StockContainerHandler<T>::operator=(M_StockContainerHandler<value_type>&& other) -> M_StockContainerHandler& {
 		this->~M_StockContainerHandler();
-		m_data = other.m_data;
-		other.m_data = nullptr;
-		if (m_data)
-			m_data->iter = this;
+		m_value = other.m_value;
+		m_container = other.m_container;
+		other.m_value = nullptr;
+		if (m_value)
+			m_container->updateCorrespondingHandler(m_value, this);
 		return *this;
 	}
 
 	template<class T>
 	M_StockContainerHandler<T>::~M_StockContainerHandler() {
-		if (m_data) {
-			m_data->value.~value_type();
-			m_data->iter = nullptr;
-		}
+		if (m_value)
+			m_container->remove(m_value);
 	}
+
 	template<class T>
 	void M_StockContainerHandler<T>::remove() {
-		if (m_data) {
-			m_data->value.~value_type();
-			m_data->iter = nullptr;
-			m_data = nullptr;
-		}
+		m_container->remove(m_value);
+		m_value = nullptr;
 	}
 
 
@@ -226,13 +173,13 @@ namespace stypox {
 	void StockContainer<T>::reserveWithoutChecking(size_type newCapacity) {
 		size_type oldSize = size();
 
-		m_first = static_cast<data_type*>(std::realloc(m_first, newCapacity * sizeof(data_type)));
-		std::memset(m_first + oldSize, 0, (newCapacity - oldSize) * sizeof(data_type));
+		m_first = static_cast<value_type*>(std::realloc(m_first, newCapacity * sizeof(value_type)));
+		m_firstHandler = static_cast<handler**>(std::realloc(m_firstHandler, newCapacity * sizeof(handler*)));
 
 		m_space = m_first + oldSize;
 		m_onePastLast = m_first + newCapacity;
 
-		updateAllIterators();
+		updateIteratorValues();
 	}
 	template<class T>
 	void StockContainer<T>::growIfNeeded() {
@@ -243,21 +190,39 @@ namespace stypox {
 	}
 
 	template<class T>
-	void StockContainer<T>::updateAllIterators() {
-		for (data_type* i = m_first; i < m_space; ++i) {
-			if (i->iter)
-				i->iter->update(i);
+	void StockContainer<T>::updateIteratorValues() {
+		for (size_t i = 0, sz = size(); i != sz; ++i)
+			m_firstHandler[i]->updateValue(m_first + i);
+	}
+	template<class T>
+	void StockContainer<T>::updateIteratorContainer() {
+		for (handler** first = m_firstHandler, ** last = m_firstHandler + (m_space - m_first); first != last; ++first)
+			(*first)->updateContainer(this);
+	}
+
+	template<class T>
+	void StockContainer<T>::remove(value_type* value) {
+		value->~value_type();
+		--m_space;
+
+		if (value != m_space) {
+			std::memcpy(value, m_space, sizeof(value_type));
+			m_firstHandler[value - m_first] = m_firstHandler[m_space - m_first];
+			m_firstHandler[value - m_first]->updateValue(value);
 		}
 	}
 
 	template<class T>
 	StockContainer<T>::StockContainer(StockContainer&& other) :
 		m_first{other.m_first}, m_space{other.m_space},
-		m_onePastLast{other.m_onePastLast} {
-		// other.m_onePastLast = nullptr
+		m_onePastLast{other.m_onePastLast}, m_firstHandler{other.m_firstHandler} {
+		updateIteratorContainer();
+
+		// other.m_onePastLast = other.m_firstHandler = nullptr
 		// ^ this is not done since the other container will not be used anymore
 		// and the destructor only needs m_first and m_space
 		other.m_first = other.m_space = nullptr;
+		other.m_firstHandler = nullptr;
 	}
 
 	template<class T>
@@ -267,41 +232,46 @@ namespace stypox {
 		m_first = other.m_first;
 		m_space = other.m_space;
 		m_onePastLast = other.m_onePastLast;
+		m_firstHandler = other.m_firstHandler;
+		
+		updateIteratorContainer();
 
 		// other.m_onePastLast = nullptr
 		// ^ this is not done since the other container will not be used anymore
 		// and the destructor only needs m_first and m_space
 		other.m_first = other.m_space = nullptr;
+		other.m_firstHandler = nullptr;
 		return *this;
 	}
 
 	template<class T>
 	StockContainer<T>::~StockContainer() {
-		for (data_type* i = m_first; i < m_space; ++i) {
-			if (i->iter)
-				i->iter->remove();
+		for (size_t i = 0, sz = size(); i != sz; ++i) {
+			m_first[i].~value_type();
+			m_firstHandler[i]->setInvalid();
 		}
 		std::free(m_first);
+		std::free(m_firstHandler);
 	}
 
 	template<class T>
 	auto StockContainer<T>::push(const value_type& value) -> handler {
 		growIfNeeded();
-		new(static_cast<void*>(&m_space->value)) value_type{value};
-		return handler{m_space++};
+		new(static_cast<void*>(m_space)) value_type{value};
+		return handler{m_space++, this};
 	}
 	template<class T>
 	auto StockContainer<T>::push(value_type&& value) -> handler {
 		growIfNeeded();
-		new(static_cast<void*>(&m_space->value)) value_type{value};
-		return handler{m_space++};
+		new(static_cast<void*>(m_space)) value_type{value};
+		return handler{m_space++, this};
 	}
 	template<class T>
 	template<class... Args>
 	auto StockContainer<T>::emplace(Args&&... value) -> handler {
 		growIfNeeded();
-		new(static_cast<void*>(&m_space->value)) value_type{value...};
-		return handler{m_space++};
+		new(static_cast<void*>(m_space)) value_type{value...};
+		return handler{m_space++, this};
 	}
 
 	template<class T>
@@ -309,9 +279,9 @@ namespace stypox {
 		reserveWithoutChecking(size() + count);
 
 		std::vector<handler> result;
-		for (size_type c = 0; c < count; ++c, ++m_space) {			
-			new(static_cast<void*>(&m_space->value)) value_type{value};
-			result.emplace_back(handler{m_space});
+		for (size_type c = 0; c < count; ++c, ++m_space) {
+			new(static_cast<void*>(m_space)) value_type{value};
+			result.push_back(handler{m_space, this});
 		}
 
 		return result;
@@ -322,9 +292,9 @@ namespace stypox {
 		reserveWithoutChecking(size() + (last - first));
 
 		std::vector<handler> result;
-		for (; first != last; ++first, ++m_space) {			
-			new(static_cast<void*>(&m_space->value)) value_type{*first};
-			result.push_back(handler{m_space});
+		for (; first != last; ++first, ++m_space) {
+			new(static_cast<void*>(m_space)) value_type{*first};
+			result.push_back(handler{m_space, this});
 		}
 
 		return result;
@@ -338,48 +308,18 @@ namespace stypox {
 	template<class T>
 	void StockContainer<T>::shrink_to_fit() {
 		if (size_type newCapacity = size(); newCapacity) {
-			m_first = static_cast<data_type*>(std::realloc(m_first, newCapacity * sizeof(data_type)));
+			m_first = static_cast<value_type*>(std::realloc(m_first, newCapacity * sizeof(value_type)));
+			m_firstHandler = static_cast<handler**>(std::realloc(m_firstHandler, newCapacity * sizeof(handler*)));
 
 			m_space = m_onePastLast = m_first + newCapacity;
 
-			updateAllIterators();
-		}
-	}
-	template<class T>
-	void StockContainer<T>::squash() {
-		data_type* fromBeginning = m_first, * fromEnd = m_space - 1;
-
-		while (1) {
-			if (fromBeginning == m_space)
-				return;
-			if (!fromBeginning->iter) {
-				while (1) {
-					if (fromBeginning == fromEnd) {
-						m_space = fromBeginning;
-						return;
-					}
-					if (fromEnd->iter) {
-						std::memcpy(fromBeginning, fromEnd, sizeof(data_type));
-						fromEnd->iter = nullptr;
-						fromBeginning->iter->update(fromBeginning);
-
-						--fromEnd;
-						if (fromBeginning == fromEnd) {
-							m_space = fromBeginning + 1;
-							return;
-						}
-						break;
-					}
-					--fromEnd;
-				}
-			}
-			++fromBeginning;
+			updateIteratorValues();
 		}
 	}
 
 	template<class T>
 	void StockContainer<T>::swap(StockContainer<value_type>& other) {
-		data_type* tmp = m_first;
+		value_type* tmp = m_first;
 		m_first = other.m_first;
 		other.m_first = tmp;
 
@@ -390,16 +330,11 @@ namespace stypox {
 		tmp = m_onePastLast;
 		m_onePastLast = other.m_onePastLast;
 		other.m_onePastLast = tmp;
-	}
 
-	template<class T>
-	auto StockContainer<T>::used() const -> size_type {
-		size_type counter = 0;
-		for (data_type* i = m_first; i < m_space; ++i) {
-			if (i->iter)
-				++counter;
-		}
-		return counter;
+		std::swap(m_firstHandler, other.m_firstHandler);
+
+		updateIteratorContainer();
+		other.updateIteratorContainer();
 	}
 }
 
